e44b77cc075c8129776682c949ebe44ca3aca135
Yugesh Palvai <yugeshpalvai21@gmail.com>
2020-08-18 13:14:26 +0530

    Generates Models for LineItem and Order and migrated to DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_064953) do
+ActiveRecord::Schema.define(version: 2020_08_18_074125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.bigint "product_id"
+    t.bigint "order_id"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_line_items_on_order_id"
+    t.index ["product_id"], name: "index_line_items_on_product_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "delivery_address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -30,5 +46,7 @@ ActiveRecord::Schema.define(version: 2020_08_18_064953) do
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
+  add_foreign_key "line_items", "orders"
+  add_foreign_key "line_items", "products"
   add_foreign_key "products", "categories"
 end
