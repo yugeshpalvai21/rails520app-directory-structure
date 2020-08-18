5710c0f59b25281122ebdddf3233c74a67bdc7ec
Yugesh Palvai <yugeshpalvai21@gmail.com>
2020-08-18 17:00:49 +0530

    Adds Index To Products name column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_111045) do
+ActiveRecord::Schema.define(version: 2020_08_18_112612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2020_08_18_111045) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_products_on_category_id"
+    t.index ["name"], name: "index_products_on_name"
   end
 
   create_table "tags", force: :cascade do |t|
