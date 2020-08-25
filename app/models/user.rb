c9e72f8f7275279306cbb0d4928325fee00d2458
Yugesh Palvai <yugeshpalvai21@gmail.com>
2020-08-25 22:42:19 +0530

    Installs and Configures rolify gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  rolify
+  after_create :assign_default_role
+
+  def assign_default_role
+    self.add_role(:site_admin)
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable, :omniauthable,
