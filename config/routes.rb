559c1806468c3293ab308e3fe054994f1457f65d
Yugesh Palvai <yugeshpalvai21@gmail.com>
2020-10-02 12:33:02 +0530

    Deep Dive - Sessions and Cookies

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations', omniauth_callbacks: 'users/callbacks' }
   root  to: 'pages#home'
+
+  get 'pages/set_cookies'
+  get 'pages/get_cookies'
+  get 'pages/clean_cookies'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
