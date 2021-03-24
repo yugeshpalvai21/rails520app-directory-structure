6ad772a78b46f292d47cf8ba252b2724b59d964d
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-03-24 14:01:19 +0530

    deep dive into rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,38 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root 'pages#home'
+
+  # get 'pages/about'
+
+  # get 'contact', to: 'pages#contact'
+  # get 'privacy-policy', to: 'pages#privacy', as: 'privacy'
+
+  # resources :articles do
+  #   resources :comments
+  # end
+
+  # resources :jobs, only: [:index] do
+  #   collection do
+  #     get 'search'
+  #   end
+
+  #   member do
+  #     get 'stats'
+  #   end
+  # end
+
+  # namespace :api do
+  #   namespace :v1 do
+  #     resources :products
+  #   end
+  # end
+
+  # scope :admin do
+  #   get 'customers', to: 'pages#customers'
+  # end
+
+  # match 'photo/:id' => 'photos#show', via: [:get, :post], as: 'show_photo'
+
+  # match '/stories' => redirect('/posts'), via: :get
+
+  # match '/', constraints: { subdomain: 'admin' }, via: :get, to: 'admin#pages'
 end
