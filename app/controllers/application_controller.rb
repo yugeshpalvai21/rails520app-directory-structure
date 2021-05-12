784cb2956fee39bdeaf1b2561bde71306f622c3c
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-05-12 20:23:05 +0530

    modifies application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   # before_action :root_redirection
 
-  def root_redirection
-    render plain: 'hey you used before action'
-  end
+  # def root_redirection
+  #   render plain: 'hey you used before action'
+  # end
 
-  def default_url_options
-    { name: 'yugesh' }
-  end
+  # def default_url_options
+  #   { name: 'yugesh' }
+  # end
 end
