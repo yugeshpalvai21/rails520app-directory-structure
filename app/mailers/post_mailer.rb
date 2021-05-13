2e978975d50ebb0fc26c5bef38322df0de47a7da
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-05-13 16:40:50 +0530

    Installs Letter Opener Gem for development environment mailer sent

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -1,7 +1,7 @@
 class PostMailer < ApplicationMailer
     #mail from: 'no-reply@organization.com'
     def send_notification
-        sleep 10
-        mail(to: 'subscriber@email.com', subject: 'New Post Created On Organization')
+        # sleep 10
+        mail(from: 'no-reply@organization.com', to: 'subscriber@email.com', subject: 'New Post Created On Organization')
     end
 end
